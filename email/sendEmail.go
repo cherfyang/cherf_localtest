3299965daab80ca61dc45a35ca009e733db55473
cherf yang <cherf.yang@bindo.com>
2025-04-17 17:51:45 +0800

    record

--- email/sendEmail.go
+++ email/sendEmail.go
@@ -5,7 +5,7 @@ import (
 	"log"
 )
 
-func sendEmail(text string) {
+func sendEmail(msg EmailMsg) {
 	// 构建邮件
 	m := gomail.NewMessage()
 
@@ -13,13 +13,14 @@ func sendEmail(text string) {
 	m.SetHeader("From", "mengxiy081@gmail.com")
 
 	// 收件人
-	m.SetHeader("To", "2637206496@qq.com")
+	m.SetHeader("To", msg.To)
 
 	// 邮件标题
-	m.SetHeader("Subject", "Go 测试邮件")
+	m.SetHeader("Subject", msg.Title)
 
 	// 邮件正文（HTML 或纯文本）
-	m.SetBody("text/html", "<h1>Hello</h1><p>这是一封来自 Go 的测试邮件。</p>")
+	m.SetBody("text/html", msg.Body)
+	//m.SetBody("text/html", "<h1>Hello</h1><p>这是一封来自 Go 的测试邮件。</p>")
 
 	// 设置 Gmail SMTP
 	d := gomail.NewDialer("smtp.gmail.com", 587, "mengxiy081@gmail.com", "cgaq kmcn octn dll v")
@@ -30,4 +31,10 @@ func sendEmail(text string) {
 	}
 
 	log.Println("发送成功")
+}
+
+type EmailMsg struct {
+	Title string
+	Body  string
+	To    string
 }
